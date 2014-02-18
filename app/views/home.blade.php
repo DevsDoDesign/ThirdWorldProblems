a50452816a4f71db93771824eaa04dcf12566567
Dan Harper <intouch@danharper.me>
2014-02-18 14:23:18 +0000

    submit page

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -8,14 +8,22 @@
 <body>
 	<section>
 		<div class="block">
-			<div class="dotted-wrapper">
+			<div class="dotted-wrapper" id="wrap">
 				<div class="hero">
 					<h1 class="submit-your">Submit Your</h1>
 					<h1 class="first-world">First World</h1>
 					<h1 class="problem">problem</h1>
 				</div>
 				<textarea name="" id="" style="height:38px !important; font-size:.8em;"></textarea>
-				<a class="button" href="">Submit</a>
+				<!--
+				<label for="decision-m">
+					Mechanical <input type="radio" id="decision-m" name="decision" value="mechanical">
+				</label>
+				<label for="decision-s">
+					Sweatshop <input type="radio" id="decision-s" name="decision" value="sweatshop">
+				</label>
+				-->
+				<a class="button" href="" id="submitBtn">Submit</a>
 			</div>
 		</div>
 	</section>
--- public/assets/js/front.js
+++ public/assets/js/front.js
@@ -1,7 +1,39 @@
 $(function() {
-	
-	$('textarea').autogrow({
 
+	var BackView = Backbone.View.extend({
+		template: function(data) {
+			var w = '';
+			if (data.weather) {
+				w = 'In '+data.weather.city+' it\'s '+data.weather.weather;
+			}
+			return 'Something... '+w;
+		},
+		render: function(data) {
+			this.$el.append(this.template(data));
+			return this;
+		}
 	});
+
+	var $wrap = $('#wrap');
+
+	var flipIt = function(text) {
+		$wrap.empty();
+		var data = {
+			text: text,
+			weather: window.TheWeather
+		};
+		var view = new BackView();
+		$wrap.append(view.render(data).el);
+	};
+
+	$('textarea').autogrow();
+
+	$('#submitBtn').click(function(e) {
+		e.preventDefault();
+		var text = $('textarea').val().trim();
+		if ( ! text) return alert("Enter something then, knob");
+		flipIt(text);
+	});
+
 
 });
