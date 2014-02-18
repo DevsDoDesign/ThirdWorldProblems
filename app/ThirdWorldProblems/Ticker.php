669cea94ef1a436c8a373c8bd22c53bbec39126f
Alasdair Smith <alasdairsmith100@gmail.com>
2014-02-18 17:58:02 +0000

    Removed template el from ticker text

--- app/ThirdWorldProblems/Ticker.php
+++ app/ThirdWorldProblems/Ticker.php
@@ -4,49 +4,57 @@ class Ticker {
 
 	protected $tickers = [
 		[
-			'el' => '<div class="block-card"><h1 class="card-intro">Since you\'ve been reading, this many people have died</h1><p class="card-counter" id="deaths">1</p><h1 class="card-insult">So shut your fucking moaning, and get on with life!</h1></div>',
+			'beforeText' => 'Since you\'ve been reading, this many people have died',
+			'afterText' => 'So shut your fucking moaning, and get on with life!',
+			'id' => 'deaths',
 			'increment' => 1,
 			'interval' => 1780,
 			'tags' => ['death']
 		],
 		[
-			'el' => '<div class="block-card"><h1 class="card-intro">Since you have been here, Oprah has made $</h1><p class="card-counter" id="oprah">1</p><h1 class="card-insult"> since you got here, pauper.</h1></div>',
+			'beforeText' => 'Since you have been here, Oprah has made',
+			'afterText' => 'since you got here, pauper',
+			'id' => 'oprah',
 			'increment' => 0.9,
 			'interval' => 100,
 			'tags' => ['oprah', 'rich', 'money']
 		],
 		[
-			'el' => '<div class="block-card"><h1 class="card-intro">Fred the Shred, makes £39 per minute on his pension. That\'s £</h1><p class="card-counter" id="fred">1</p><h1 class="card-insult"> since you loaded the page</h1></div>',
+			'beforeText' => 'Fred the Shred, makes £39 per minute on his pension. That\'s',
+			'afterText' => 'since you loaded the page',
+			'id' => 'fred',
 			'increment' => 0.065,
 			'interval' => 100,
 			'tags' => ['fred', 'money', 'rich']
 		],
 		// [
-		// 	'el' => '<div class="block-card"><h1 class="card-intro">Since you\'ve been reading, this many people have got divorced</h1><p class="card-counter" id="divorce">1</p><h1 class="card-insult">Being single doesn\'t look so bad now does it?</h1></div>',
-		// 	'increment' => 0.2,
-		// 	'interval' => 1300,
-		// 	'tags' => ['divorce']
-		// ],
-		// [
-		// 	'el' => '<div class="block-card"><h1 class="card-intro">Since you\'ve been reading, this many people have got divorced</h1><p class="card-counter" id="divorce">1</p><h1 class="card-insult">Being single doesn\' look so bad now does it?</h1></div>',
+		// 	'beforeText' => 'Since you\'ve been reading, this many people have got divorced',
+		// 	'afterText' => 'Being single doesn\'t look so bad now does it?',
+		// 	'id' => 'divorce',
 		// 	'increment' => 0.2,
 		// 	'interval' => 1300,
 		// 	'tags' => ['divorce']
 		// ],
 		[
-			'el' => '<div class="block-card"><h1 class="card-intro">Since you\'ve been reading, this many people have been diagnosed with HIV</h1><p class="card-counter" id="hiv">1</p><h1 class="card-insult">Wear a condom, dickhead.</h1></div>',
+			'beforeText' => 'Since you\'ve been reading, this many people have been diagnosed with HIV',
+			'afterText' => 'Wear a condom, dickhead.',
+			'id' => 'hiv',
 			'increment' => 1,
 			'interval' => 6600,
 			'tags' => ['hiv', 'sick', 'ill']
 		],
 		// [
-		// 	'el' => '<div class="block-card"><h1 class="card-intro">Dan Edwards, Young Designer of the Year has had</h1><p class="card-counter" id="de">1</p><h1 class="card-insult">spam mentions on Twitter since you got here.</h1></div>',
+		// 	'beforeText' => 'Dan Edwards, Young Designer of the Year has had',
+		// 	'afterText' => 'spam mentions on Twitter since you got here.',
+		// 	'id' => 'de',
 		// 	'increment' => 1,
 		// 	'interval' => 5000,
 		// 	'tags' => ['de', 'dan', 'design']
 		// ],
 		[
-			'el' => '<div class="block-card"><h1 class="card-intro">Justin Beiber has gained this many fans since</h1><p class="card-counter" id="justin">1</p><h1 class="card-insult">Don\'t you wish someone loved you that much?</h1></div>',
+			'beforeText' => 'Justin Beiber has gained this many fans since',
+			'afterText' => 'Don\'t you wish someone loved you that much?',
+			'id' => 'justin',
 			'increment' => 1,
 			'interval' => 2000,
 			'tags' => ['justin', 'bieber']
