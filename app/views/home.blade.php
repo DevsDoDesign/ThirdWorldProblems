7e61ac4b007fc21137f9e87806dff08423a03fb2
Robb Lewis <me@therobb.com>
2014-02-18 11:59:44 +0000

    Moar sad facts

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,4 +2,10 @@ Hello!
 
 <p><span id="deaths">0</span> people have died since you read this, you fuckwit.</p>
 
+<p>Oprah has made $<span id="oprah">0</span> since you got here, pauper.</p>
+
+<p><span id="divorce">2</span> people have got divorced. Being single doesn't look so bad now does it?</p>
+
+<p><span id="hiv">0</span> have been diagnosed with HIV. Wear a condom, dickhead.</p>
+
 @include('_app')
--- public/assets/js/ticker.js
+++ public/assets/js/ticker.js
@@ -1,5 +1,5 @@
 
-var runTicker = function() {
+var runDeaths = function() {
 	var totalDeaths = 0, $deaths = $('#deaths');
 	var ticker = function() {
 		totalDeaths++;
@@ -7,7 +7,37 @@ var runTicker = function() {
 	};
 	setInterval(ticker, 1780);
 };
+
+var runOprah = function() {
+	var oprahMade = 0, $oprah = $('#oprah');
+	var ticker = function() {
+		oprahMade += 9;
+		$oprah.text(oprahMade);
+	};
+	setInterval(ticker, 1000);
+};
+
+var runDivorce = function() {
+	var divorces = 2, $divorce = $('#divorce');
+	var ticker = function() {
+		divorces += 2;
+		$divorce.text(divorces);
+	};
+	setInterval(ticker, 13000);
+};
+
+var runHiv = function() {
+	var hiv = 0, $hiv = $('#hiv');
+	var ticker = function() {
+		hiv += 1;
+		$hiv.text(hiv);
+	};
+	setInterval(ticker, 6600);
+};
 	
 $(function() {
-	runTicker();
+	runDeaths();
+	runOprah();
+	runDivorce();
+	runHiv();
 });
